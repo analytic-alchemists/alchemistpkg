44786b61d7eed385988fc32ec8a481da2c1bfe19
MILAN BHAKTA <milzbhakta@gmail.com>
2024-01-13 18:31:06 -0500

    Include configs/*yml files into package to be used

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
-
+from setuptools import find_packages
 setup(
     name='alchemistpkg',
     version='0.1.0',
     author='Analytic Alchemists',
-    packages=['alchemistpkg'],
+    packages=['alchemistpkg', 'configs'],
+    include_package_data=True,
     install_requires=['matplotlib', 'requests', 'pyyaml']
 )
 
