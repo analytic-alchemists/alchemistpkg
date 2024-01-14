7beee52ae14cc7e4d3790e540b9019410ef0faae
MILAN BHAKTA <milzbhakta@gmail.com>
2024-01-13 19:09:42 -0500

    Fix the setup file to include config in alchemistpkg not as a seperate package

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='alchemistpkg',
     version='0.1.0',
     author='Analytic Alchemists',
-    packages=['alchemistpkg'],
+    packages=find_packages(),
+    include_package_data=True,
     install_requires=['matplotlib', 'requests', 'pyyaml']
 )
 
